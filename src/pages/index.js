dab04a8984a14379c6bb00208aeb476705519f0b
bilalhussaindev <hellobilalhussain@gmail.com>
2024-05-02 14:26:15 +0500

    edit Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,55 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Logo from "@site/static/img/logo.svg";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+
+	return (
+		<header className={clsx("hero", styles.heroBanner)}>
+			<div className="container">
+				<div className="text--center">
+					<Logo className={styles.logo} role="img" />
+				</div>
+
+				<Heading as="h1" className="hero__title">
+					{siteConfig.title}
+				</Heading>
+
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+
+				<div className={styles.buttons}>
+					<Link
+						className="button button--primary button--lg"
+						to="/docs/category/quick-start"
+					>
+						Learn React
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+	const { siteConfig } = useDocusaurusContext();
+
+	return (
+		<Layout
+			title={siteConfig.title}
+			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+		>
+			<HomepageHeader />
+
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
 }
